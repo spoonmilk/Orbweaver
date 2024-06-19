fdb21e96d611b53634c81035f90b83a4efd64a78
Tanish Makadia <tanishmakadia@gmail.com>
2024-06-19 15:28:06 -0400

    chore: Working React, TypeScript, TailwindCSS, and postcss boilerplate

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
+      <h1 className="text-xl font-medium text-black">
+        Hello, Chrome Extension!
+      </h1>
+      <p className="text-gray-500">
+        This is a React component using Tailwind CSS.
+      </p>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,40 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const config = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
+
+export default config;
